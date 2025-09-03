48e8d45277ff132b05bd901bf4cea7d4c673339a
Eduardo <eduardoluisalonsi@gmail.com>
2025-09-03 18:04:44 -0300

    oi

---
+++ javascript/galeria-data.js
@@ -0,0 +1,468 @@
+const galeriaData = {
+    "2022": [
+        {
+            src: "images/2022/2022_foto1.webp",
+            titulo: "Aula",
+            data: "2022",
+            alt: "Aula do Pensamento Computacional"
+        },
+        {
+            src: "images/2022/2022_foto2.webp",
+            titulo: "Aula",
+            data: "2022",
+            alt: "Aula do Pensamento Computacional"
+        },
+        {
+            src: "images/2022/2022_foto3.webp",
+            titulo: "Aula",
+            data: "2022",
+            alt: "Aula do Pensamento Computacional"
+        },
+        {
+            src: "images/2022/2022_foto4.webp",
+            titulo: "Coordenador do curso, Sergio Tanaka",
+            data: "2022",
+            alt: "Coordenador do curso, Sergio Tanaka"
+        },
+        {
+            src: "images/2022/2022_foto5.webp",
+            titulo: "Monitores pensamento computacional de 2022",
+            data: "2022",
+            alt: "Monitores pensamento computacional de 2022"
+        },
+        {
+            src: "images/2022/2022_foto6.webp",
+            titulo: "Alunos de 2022 na escada",
+            data: "2022",
+            alt: "Alunos do pensamento computacional de 2022 na escada"
+        },
+        {
+            src: "images/2022/2022_foto7.webp",
+            titulo: "Melhores alunos do Pensamento Computacional de 2022",
+            data: "2022",
+            alt: "Melhores alunos do Pensamento Computacional de 2022 - matéria"
+        }
+        
+    ],
+    "2023": [
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Alunos 2023 na escada",
+            data: "2023",
+            alt: "Estudantes em escada"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Atividade em Grupo",
+            data: "Março 2023",
+            alt: "Alunos participando de atividade em grupo"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Aula Prática",
+            data: "Março 2023",
+            alt: "Aula prática com demonstração"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Professores e reitor da universidade",
+            data: "Março 2023",
+            alt: "Professores e reitor da universidade"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Apresentação",
+            data: "Março 2023",
+            alt: "Apresentação"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Aula",
+            data: "2023",
+            alt: "Aula"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Aula",
+            data: "2023",
+            alt: "Aula"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Aula",
+            data: "2023",
+            alt: "Aula"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Aula",
+            data: "2023",
+            alt: "Aula"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Aula",
+            data: "2023",
+            alt: "Aula"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Aula",
+            data: "2023",
+            alt: "Aula"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Monitores 2023",
+            data: "2023",
+            alt: "Monitores 2023"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Aula",
+            data: "2023",
+            alt: "Aula"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Aula",
+            data: "2023",
+            alt: "Aula"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Aula",
+            data: "2023",
+            alt: "Aula"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Aula",
+            data: "2023",
+            alt: "Aula"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Aula",
+            data: "2023",
+            alt: "Aula"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Aula",
+            data: "2023",
+            alt: "Aula"
+        },
+        {
+            src: "images/2023/2023_foto1.jpg",
+            titulo: "Turma G - Primeiro Dia",
+            data: "11 Maio 2023",
+            alt: "Turma G no primeiro dia de atividades"
+        }
+    ],
+"2024": [
+    {
+        src: "img/2024/DSC_7697.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7700.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7705.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7707.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7711.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7717.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7720.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+
+    {
+        src: "img/2024/DSC_7747.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7787.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7846.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7859.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7869.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7876.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7893.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7908.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7911.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7915.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7987.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7991.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_7998.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8062.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8070.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8075.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8077.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8103.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8121.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8125.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8127.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8129.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8141.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8144.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8148.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8154.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8158.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8164.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8170.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8175.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8179.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8196.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    },
+    {
+        src: "img/2024/DSC_8240.webp",
+        titulo: "Formatura 2024",
+        data: "2024",
+        alt: "Formatura Pensamento Computacional de 2024"
+    }
+    ],
+    "2025": [
+        {
+            src: "img/2025/pensamento1.JPG",
+            titulo: "Aula",
+            data: "2025",
+            alt: "Aula de Pensamento Computacional de 2025"
+        },
+        {
+            src: "img/2025/pensamento2.JPG",
+            titulo: "Aula",
+            data: "2025",
+            alt: "Aula de Pensamento Computacional de 2025"
+        },
+        {
+            src: "img/2025/pensamento3.JPG",
+            titulo: "Aula",
+            data: "2025",
+            alt: "Aula de Pensamento Computacional de 2025"
+        },
+        {
+            src: "img/2025/pensamento4.JPG",
+            titulo: "Aula",
+            data: "2025",
+            alt: "Aula de Pensamento Computacional de 2025"
+        },
+        {
+            src: "img/2025/pensamento5.JPG",
+            titulo: "Aula",
+            data: "2025",
+            alt: "Aula de Pensamento Computacional de 2025"
+        },
+        {
+            src: "img/2025/pensamento6.JPG",
+            titulo: "Aula",
+            data: "2025",
+            alt: "Aula de Pensamento Computacional de 2025"
+        },
+        {
+            src: "img/2025/pensamento7.JPG",
+            titulo: "Aula",
+            data: "2025",
+            alt: "Aula de Pensamento Computacional de 2025"
+        },
+        {
+            src: "img/2025/pensamento8.JPG",
+            titulo: "Aula",
+            data: "2025",
+            alt: "Aula de Pensamento Computacional de 2025"
+        },
+        {
+            src: "img/2025/pensamento9.JPG",
+            titulo: "Aula",
+            data: "2025",
+            alt: "Aula de Pensamento Computacional de 2025"
+        },
+        {
+            src: "img/2025/pensamento10.JPG",
+            titulo: "Aula",
+            data: "2025",
+            alt: "Aula de Pensamento Computacional de 2025"
+        }
+    ]
+};
